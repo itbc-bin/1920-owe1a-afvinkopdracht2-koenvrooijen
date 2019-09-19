0a202b625d98dd117f6d294cab3bab066c15b3eb
koenvrooijen <55191689+koenvrooijen@users.noreply.github.com>
2019-09-19 11:40:41 +0200

    Add files via upload

--- gcperc_berekenen.py
+++ gcperc_berekenen.py
@@ -1,8 +1,14 @@
+#bij test voer je de sequentie in
+
 test = "cgatcgatgctagctgacgatgtggtaggcttgactgcg"
+# vervolgens print hij de hoeveelheid van elke letter
+
 print(test.count("a"))
 print(test.count("t"))
 print(test.count("c"))
 print(test.count("g"))
+
+#en dan print hij het totaal van de gc en het percentage
 
 print (test.count("g")+ (test.count("c")))
 
